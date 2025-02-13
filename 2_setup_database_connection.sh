bee3489d80f6b18168856c52112dd0d440c8d189
Adam Bako <bakoad@gmail.com>
2025-02-13 07:55:05 +0100

    add restore of og config

--- 2_setup_database_connection.sh
+++ 2_setup_database_connection.sh
@@ -7,7 +7,7 @@ python -m venv .openmetadata_venv
 source .openmetadata_venv/bin/activate
 pip install -r requirements.txt
 
-echo -e "\n=== Updating Database Connection In OpenMetadata ==="
+echo -e "\n=== Setting Database Connection In OpenMetadata ==="
 ENCODED_PASSWORD=$(echo -n "admin" | base64)
 JWT_TOKEN=$(curl -X POST http://localhost:8585/api/v1/users/login \
   -H 'Content-Type: application/json' \
@@ -16,3 +16,6 @@ JWT_TOKEN=$(curl -X POST http://localhost:8585/api/v1/users/login \
 cp openmetadata/database_connection.yaml openmetadata/database_connection.yaml.original
 sed "s/{jwt_token}/$JWT_TOKEN/" openmetadata/database_connection.yaml.original > openmetadata/database_connection.yaml
 python -m metadata ingest -c openmetadata/database_connection.yaml
+
+echo -e "\n=== Restoring Original Configuration ==="
+mv openmetadata/database_connection.yaml.original openmetadata/database_connection.yaml
